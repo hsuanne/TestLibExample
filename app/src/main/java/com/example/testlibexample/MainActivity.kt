d11b53ed2702550ce5edeb52ee56ad70b05e2bac
hsuanne <rabbit222nini@gmail.com>
2023-02-13 10:40:55 +0800

    delete submodule

--- app/src/main/java/com/example/testlibexample/MainActivity.kt
+++ app/src/main/java/com/example/testlibexample/MainActivity.kt
@@ -2,14 +2,10 @@ package com.example.testlibexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.toasterlibrary.ToasterMessage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        ToasterMessage.toastMyMessage(this, "Successfully import library!!!")
-        ToasterMessage.logMyMessage("Successfully import library!!!")
     }
 }
