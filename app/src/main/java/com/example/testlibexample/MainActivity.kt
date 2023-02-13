7e6e71929df4d64981b386a568a16528e7db2712
hsuanne <rabbit222nini@gmail.com>
2023-02-13 15:58:45 +0800

    add bluetooth-demo bt library

--- app/src/main/java/com/example/testlibexample/MainActivity.kt
+++ app/src/main/java/com/example/testlibexample/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.testlibexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.btlibrary.Toaster
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toaster.showToast(this, "Successfully import btlibrary!")
     }
 }
